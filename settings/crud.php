0e9c86bcadc330c62f098f9fa22c4e87d210c872
Zehra Alizada <zehra.alizada@ibp.az>
2023-05-02 10:37:50 +0400

    commit

--- settings/crud.php
+++ settings/crud.php
@@ -1,6 +1,11 @@
 <?php
 
+// service inserti
 
+$insert = getInsert('users',['name','image','title'],[$_POST['sadiq'],'http','basliq']);
+if ($insert){
+    header("Location: ");
+}
 
 
 
--- settings/functions.php
+++ settings/functions.php
@@ -2,6 +2,17 @@
 
 include "db.php";
 
+function getInsert(string $tName,array $columnName,array $values): bool
+{
+    return true;
+}
 
+$insert = getInsert('services',['name','image','title'],['sadiq','http','basliq']);
+if ($insert){
 
+}
+$sql = "INSERT INTO about (header,image,content) VALUES (?,?,?)";
+$insert = $conn->prepare($sql);
+$insert->execute([$about_header,$about_image,$about_description]);
+header("Location:add_about.php");
 ?>
